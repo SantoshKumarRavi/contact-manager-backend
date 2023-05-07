881a1bfecab122db2d9a216b974713aba2848c91
Santhosh kumar <drsanthosh1997@gmail.com>
2023-05-07 17:39:39 +0530

    all code added in routes

--- contactmanagerback/src/index.js
+++ contactmanagerback/src/index.js
@@ -1,11 +1,15 @@
 const express = require("express");
 const app = express();
-const port = 8081;
+const port = process.env.PORT || 3000;
 const mongoose = require("mongoose");
 const contact = require("../datamodel/contacts");
 const bodyParser = require("body-parser");
-const routes = require("../routes/userRoutes");
-app.use(routes);
+const auth_routes = require("../routes/authentication/authenticate");
+const contacts_routes=require("../routes/contacts/contacts")
+require('dotenv').config();
+
+app.use(auth_routes);
+app.use(contacts_routes);
 
 app.use(bodyParser.urlencoded({ extended: false }));
 
@@ -19,67 +23,10 @@ app.use(
   })
 );
 
-app.post("/contacts", async (req, res) => {
-  let updated = req.body.data;
-  let id = req.body.userId;
-  for (let i = 0; i < updated.length; i++) {
-    let objUpdate = updated[i];
-    objUpdate.UserId = id;
-    updated[i] = objUpdate;
-  }
-  await contact.insertMany(updated, function (err, datas) {
-    if (err) {
-      console.log(err);
-    }
-    res.send(JSON.stringify({ message: "sucessfully saved", data: datas }));
-  });
-});
-
-app.get("/contacts/:id", async (req, res) => {
-  let paramId = req.params["id"];
-  const strid = paramId.valueOf();
-  await contact
-    .find({ UserId: strid }, function (err, datas) {
-      if (err) {
-        console.log(err);
-      }
-      res.send(JSON.stringify({ message: "sucessfully saved", data: datas }));
-    }).clone();
-});
-
-app.delete("/contacts", async (req, res) => {
-  let deleteIdArray = req.body;
-  deleteIdArray = deleteIdArray.map((x) => mongoose.Types.ObjectId(x));
-  await contact
-    .deleteMany({ _id: { $in: deleteIdArray } }, function (err, delCount) {
-      if (err) {
-        console.log(err);
-      }
-      res.send(
-        JSON.stringify({ message: "sucessfully deletes", data: delCount })
-      );
-    }).clone();
-});
-
-//this is now i used for postman deleteall ...
-app.delete("/emptycontacts", async (req, res) => {
-  //temporary api call for delete // by putting if req.body,we can make one api call for delete and delerte all..
-  await contact
-    .deleteMany({}, function (err, delCount) {
-      if (err) {
-        console.log(err);
-      }
-      res.send(
-        JSON.stringify({ message: "sucessfully deleted all", data: delCount })
-      );
-    }).clone();
-});
-
-app.listen(process.env.PORT || 3000, () => {
+app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
 });
-
 mongoose.connect(
-    process.env.MONGODB_URI
+process.env.MONGODB_URI
   )
   .then(() => console.log("db connected"));
